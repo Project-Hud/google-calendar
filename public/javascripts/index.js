e9a11b92c8321a19095252ee59b04d9e0530e701
James Mortemore <james.mortemore@clock.co.uk>
2014-01-08 17:04:05 +0000

    Fixed busy issue, now takes into account start time. Reduced checks from 30 minutes to every 30 seconds, google has a limit of 100,000 per day

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,6 @@
 (function () {
-  var services = 0
-    , okServices = 0
-    , $iconSuccess = document.getElementsByClassName('js-success')
+  var $iconSuccess = document.getElementsByClassName('js-success')
     , $iconDanger = document.getElementsByClassName('js-danger')
-    , $iconLoading = document.getElementsByClassName('js-loading')
     , $textSuccess = document.getElementsByClassName('js-free')
     , $textDanger = document.getElementsByClassName('js-busy')
     , $textFreeMins = document.getElementsByClassName('js-free-mins')
@@ -12,32 +9,47 @@
   $iconDanger[0].style.display = 'none'
 
   document.getElementsByClassName('js-busy')[0].style.display = 'none'
-    document.getElementsByClassName('js-free')[0].style.display = 'none'
+  document.getElementsByClassName('js-free')[0].style.display = 'none'
+
+  function setFree() {
+    $iconSuccess[0].style.display = 'block'
+    $textSuccess[0].style.display = 'block'
+
+    $iconDanger[0].style.display = 'none'
+    $textDanger[0].style.display = 'none'
+  }
+
+  function setBusy(minutes) {
+    $iconDanger[0].style.display = 'block'
+    $textDanger[0].style.display = 'block'
+
+    $iconSuccess[0].style.display = 'none'
+    $textSuccess[0].style.display = 'none'
+
+    $textFreeMins[0].innerHTML = minutes
+  }
 
   var request = new XMLHttpRequest()
   request.onreadystatechange = function () {
+    if (request.readyState !== 4 || request.status !== 200) return
+
     var res = JSON.parse(request.response)
 
     if(res.busy.length === 0) {
-      $iconSuccess[0].style.display = 'block'
-      $textSuccess[0].style.display = 'block'
-
-      $iconDanger[0].style.display = 'none'
-      $textDanger[0].style.display = 'none'
+      setFree()
     } else {
-      $iconDanger[0].style.display = 'block'
-      $textDanger[0].style.display = 'block'
 
-      $iconSuccess[0].style.display = 'none'
-      $textSuccess[0].style.display = 'none'
+      var startDate = new Date(res.busy[0].start)
+        , now = new Date()
+
+      if (startDate > now) return setFree()
 
       var endDate = new Date(res.busy[0].end)
-        , now = new Date()
         , difference = endDate - now
         , seconds = Math.floor((difference) / 1000)
         , minutes = Math.floor(seconds / 60)
 
-      $textFreeMins[0].innerHTML = minutes
+      setBusy(minutes)
 
     }
   }
@@ -48,6 +60,6 @@
   setInterval(function () {
     request.open('GET', '/calendar-status', true)
     request.send()
-  }, 300000)
+  }, 30000) // 30 seconds
 
 })()
